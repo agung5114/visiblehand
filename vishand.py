0e2c5178e943f36e1b60f4bcc255420716bee7d1
agung5114 <43102810+agung5114@users.noreply.github.com>
2021-08-22 21:13:06 +0700

    Update vishand.py

--- vishand.py
+++ vishand.py
@@ -21,7 +21,8 @@ from pulp import LpMaximize, LpProblem, LpStatus, lpSum, LpVariable
 
 def main():
     # st.title("Simulasi Realokasi Budget untuk memaksimalkan Efisiensi dan Omset")
-    # st.sidebar
+    greach= Image.open('GReach.png')
+    st.sidebar.image(greach)
     # menu = ["Sasaran Penerima Bantuan","Sebaran Program Pemerintah","Daftar Kandidat Penerima Bantuan","Pendaftaran UMKM"]
     menu = ['MSME Report and Analysis',"Government Benefits",'MSME Candidate List']
     choice = st.sidebar.selectbox("Select Menu", menu)
